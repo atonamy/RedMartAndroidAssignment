c1689bd6f7c0da6660c511bb35d010d6cce3f5d1
archie <archie@coinpip.com>
2016-01-30 10:47:30 +0800

    added volley manager

---
+++ app/src/main/java/com/redmart/redmartandroidassignment/VolleyManager.java
@@ -0,0 +1,22 @@
+package com.redmart.redmartandroidassignment;
+
+import android.content.Context;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by archie on 29/1/16.
+ */
+public class VolleyManager {
+    private static RequestQueue volleyQueue = null;
+
+    public static void init(Context context) {
+        if(volleyQueue == null)
+            volleyQueue =  Volley.newRequestQueue(context);
+    }
+
+    public static RequestQueue getVolleyQueue() {
+        return volleyQueue;
+    }
+}

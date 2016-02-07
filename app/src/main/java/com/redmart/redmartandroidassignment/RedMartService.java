e94e9a37f474af52de4168dc6ad799d20be67fee
archie <archie@coinpip.com>
2016-02-07 11:45:33 +0800

    crash app fix

--- app/src/main/java/com/redmart/redmartandroidassignment/RedMartService.java
+++ app/src/main/java/com/redmart/redmartandroidassignment/RedMartService.java
@@ -424,6 +424,8 @@ public class RedMartService {
                     else
                         isComplete = true;
                 }
+                else
+                    isComplete = true;
             }
             else
                 isComplete = true;
